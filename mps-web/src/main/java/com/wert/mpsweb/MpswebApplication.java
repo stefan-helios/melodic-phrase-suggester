49b3c33a7cf8c811a35b5faf9d9a5eb8485efe2f
stefan-helios <stefan@go-helios.com>
2024-07-10 18:02:01 -0400

    Recreate Spring Web project

---
+++ mps-web/src/main/java/com/wert/mpsweb/MpswebApplication.java
@@ -0,0 +1,13 @@
+package com.wert.mpsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpswebApplication.class, args);
+	}
+
+}
---
+++ mps-web/src/test/java/com/wert/mpsweb/MpswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wert.mpsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
